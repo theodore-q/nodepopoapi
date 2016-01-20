7be5c0c926c6ff130ea53057482ca4e5436bfca1
Theo <theo.q.er@gmail.com>
2016-01-20 22:24:49 +0000

    make it better

---
+++ app.js
@@ -0,0 +1,34 @@
+(function() {
+  var http, server;
+
+  var qs = require('querystring');
+  http = require('http');
+
+  server = http.createServer().listen(8080);
+
+  server.on('request', function(request, response) {
+    var callback, callbackName, options, querystring,latitude,longitude;
+    querystring = require('url').parse(request.url, true);
+    callbackName = querystring.query['callback'];
+    longitude = querystring.query['lng'];
+    latitude = querystring.query['lat'];
+    options = {
+      host: 'data.police.uk',
+      path: '/api/crimes-street/all-crime?lat=' + latitude + '&lng=' + longitude
+    };
+    callback = function(rsp) {
+      response.write("" + callbackName + "(");
+      rsp.on('data', function(chunk) {
+        return response.write(chunk.toString());
+      });
+      console.log(callbackName);
+      return rsp.on('end', function() {
+        return response.end(')');
+      });
+    };
+    console.log(longitude);
+    console.log(latitude);
+    return http.request(options, callback).end();
+  });
+
+}).call(this);
